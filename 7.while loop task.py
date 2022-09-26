75f569f1aa1a9dc70d4714b9b16d02410acfb97f
NRjadav <109718224+NRjadav@users.noreply.github.com>
2022-09-26 15:50:56 -0700

    Python

---
+++ 7.while loop task.py
@@ -0,0 +1,26 @@
+
+N=True
+sum=0
+
+while N:
+    num=int(input("enter a number:"))
+    sum+=num
+    choice=("'y'for yes 'n' for no")
+    if choice=='y':
+        N=True
+    else:
+        N=False
+print(sum)                
+            
+
+
+
+
+
+
+
+
+
+
+
+
